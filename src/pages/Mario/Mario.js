17f7b18f67a8009f1e7015432a70967f2ce5c392
Martijn <martijncvv@gmail.com>
2021-06-29 16:05:42 +0200

    added mario game levels

--- src/pages/Mario/Mario.js
+++ src/pages/Mario/Mario.js
@@ -13,6 +13,8 @@ kaboom({
 	REACT RED <a href="https://imgbb.com/"><img src="https://i.ibb.co/tmMjn9S/1-JSFjofdj-IH5-RDVf-q-OODGw.png" alt="1-JSFjofdj-IH5-RDVf-q-OODGw" border="0"></a>
 	REDUX <a href="https://ibb.co/X43ffQ8"><img src="https://i.ibb.co/QcC77gd/redux-logo.png" alt="redux-logo" border="0"></a>
 	CODAISSEUR LOGO<a href="https://imgbb.com/"><img src="https://i.ibb.co/LZBZBdQ/codaisseur-square.png" alt="codaisseur-square" border="0"></a>
+	JS LOGO <a href="https://imgbb.com/"><img src="https://i.ibb.co/9gKw5x1/js-logo.png" alt="js-logo" border="0"></a>
+	DB LOGO <a href="https://ibb.co/Zhv72T0"><img src="https://i.ibb.co/FBcCH7F/database-logo.png" alt="database-logo" border="0"></a>
 	*/
 }
 
@@ -33,9 +35,11 @@ loadSprite(
 loadSprite("codaisseur-logo", "https://i.ibb.co/LZBZBdQ/codaisseur-square.png");
 loadSprite("evil-redux", "https://i.ibb.co/QcC77gd/redux-logo.png");
 loadSprite("good-react", "https://i.ibb.co/PTQStFY/reactjs-logo.png");
+loadSprite("block", "https://i.ibb.co/9gKw5x1/js-logo.png");
+loadSprite("DB-pipe", "https://i.ibb.co/FBcCH7F/database-logo.png");
+
 loadRoot("https://i.imgur.com/");
 loadSprite("brick", "pogC9x5.png");
-loadSprite("block", "M6rwarW.png");
 
 loadSprite("surprise", "gesQ1KP.png");
 loadSprite("unboxed", "bdrLpi6.png");
@@ -51,7 +55,7 @@ loadSprite("blue-surprise", "RMqCc1G.png");
 // loadSprite("evil-react", "KPO3fR9.png");
 // loadSprite("blue-evil-react", "SvV4ueD.png");
 
-scene("game", ({ level, score }) => {
+scene("game", ({ level, score, levelNumber }) => {
 	layers(["bg", "obj", "ui"], "obj");
 
 	const maps = [
@@ -62,7 +66,7 @@ scene("game", ({ level, score }) => {
 			"                                                                                            ",
 			"                                                                                           ",
 			"             %*      **                                                                      ",
-			"             ^                  ^                                                             ",
+			"      p       ^                  ^                                                             ",
 			"     ===    ====    ====       ==      =   ===    ===    ==%*   =    =  ===                        ",
 			"   ==      =    =   =  ==     =  =        =   =  =   =   =      =    =  =  =                       ",
 			"   =      =      =  =   =     =  =     =  =      =       =      =    =  =   =                      ",
@@ -82,7 +86,7 @@ scene("game", ({ level, score }) => {
 			"     %   =*=%=                        ",
 			"                                      ",
 			"                            -+        ",
-			"                    ^   ^   ()        ",
+			"   p                ^   ^   ()        ",
 			"=========      ===============   =====",
 			"         ====                         ",
 		],
@@ -95,7 +99,7 @@ scene("game", ({ level, score }) => {
 			"£        @@@@@@              x x        £",
 			"£                          x x x        £",
 			"£                        x x x x  x   -+£",
-			"£               z   z  x x x x x  x   ()£",
+			"£    p           z   z  x x x x x  x   ()£",
 			"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
 		],
 	];
@@ -103,7 +107,7 @@ scene("game", ({ level, score }) => {
 	const levelCfg = {
 		width: 20,
 		height: 20,
-		"=": [sprite("block"), solid()],
+		"=": [sprite("block"), scale(0.05), solid()],
 		"^": [sprite("evil-react"), solid(), scale(0.08), "dangerous", body()],
 		"#": [sprite("good-react"), solid(), scale(0.05), "good-react", body()],
 		z: [sprite("evil-redux"), solid(), scale(0.05), "dangerous", body()],
@@ -111,13 +115,14 @@ scene("game", ({ level, score }) => {
 		"%": [sprite("surprise"), solid(), "coin-surprise"],
 		"*": [sprite("surprise"), solid(), "react-surprise"],
 		"}": [sprite("unboxed"), solid()],
-		"(": [sprite("pipe-bottom-left"), solid(), scale(0.5)],
-		")": [sprite("pipe-bottom-right"), solid(), scale(0.5)],
-		"-": [sprite("pipe-top-left"), solid(), scale(0.5), "pipe"],
-		"+": [sprite("pipe-top-right"), solid(), scale(0.5), "pipe"],
+		// "(": [sprite("pipe-bottom-left"), solid(), scale(0.5)],
+		// ")": [sprite("pipe-bottom-right"), solid(), scale(0.5)],
+		// "-": [sprite("pipe-top-left"), solid(), scale(0.5), "pipe"],
+		// "+": [sprite("pipe-top-right"), solid(), scale(0.5), "pipe"],
 		"!": [sprite("blue-block"), solid(), scale(0.5)],
 		"£": [sprite("blue-brick"), solid(), scale(0.5)],
 		"@": [sprite("blue-surprise"), solid(), scale(0.5), "coin-surprise"],
+		p: [sprite("DB-pipe"), solid(), scale(0.04), "DB-pipe"],
 		x: [sprite("blue-steel"), solid(), scale(0.5)],
 	};
 
@@ -125,14 +130,24 @@ scene("game", ({ level, score }) => {
 
 	const scoreLabel = add([
 		text(score),
-		pos(30, 6),
+		pos(80, 20),
 		layer("ui"),
 		{
 			value: score,
 		},
 	]);
 
-	add([text("level " + parseInt(level + 1)), pos(40, 6)]);
+	const levelLabel = add([
+		text(levelNumber),
+		pos(80, 6),
+		layer("ui"),
+		{
+			value: levelNumber,
+		},
+	]);
+
+	add([text("Score "), pos(30, 20)]);
+	add([text("Level "), pos(30, 6)]);
 
 	function big() {
 		let timer = 0;
@@ -174,6 +189,18 @@ scene("game", ({ level, score }) => {
 		scale(0.07),
 	]);
 
+	player.collides("DB-pipe", () => {
+		keyPress("down", () => {
+			levelLabel.value++;
+			levelLabel.text = levelLabel.value;
+			go("game", {
+				level: (level + 1) % maps.length,
+				score: scoreLabel.value,
+				levelNumber: levelNumber + 1,
+			});
+		});
+	});
+
 	action("good-react", (m) => {
 		m.move(20, 0);
 	});
@@ -194,6 +221,8 @@ scene("game", ({ level, score }) => {
 	player.collides("good-react", (m) => {
 		destroy(m);
 		camShake(25);
+		scoreLabel.value++;
+		scoreLabel.text = scoreLabel.value;
 		player.biggify(6);
 	});
 
@@ -225,15 +254,6 @@ scene("game", ({ level, score }) => {
 		}
 	});
 
-	player.collides("pipe", () => {
-		keyPress("down", () => {
-			go("game", {
-				level: (level + 1) % maps.length,
-				score: scoreLabel.value,
-			});
-		});
-	});
-
 	keyDown("left", () => {
 		player.move(-MOVE_SPEED, 0);
 	});
@@ -260,4 +280,4 @@ scene("lose", ({ score }) => {
 	add([text(score, 32), origin("center"), pos(width() / 2, height() / 2)]);
 });
 
-start("game", { level: 0, score: 0 });
+start("game", { level: 0, score: 0, levelNumber: 0 });
